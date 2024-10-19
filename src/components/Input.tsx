615292a2276391c69ca0979d9ee9b62a9a304d6a
ixedasan <pandikgenshin@gmail.com>
2024-10-20 00:04:48 +0300

    refactor(Input): improve readability and add memo

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,9 @@
-'use client'
-
+import { memo } from 'react'
 import { type useChat } from 'ai/react'
 import { Send } from 'lucide-react'
-import { Button } from "./ui/button"
-import { Textarea } from "./ui/textarea"
+
+import { Button } from './ui/button'
+import { Textarea } from './ui/textarea'
 
 type HandleInputChange = ReturnType<typeof useChat>['handleInputChange']
 type HandleSubmit = ReturnType<typeof useChat>['handleSubmit']
@@ -16,47 +16,51 @@ interface ChatInputProps {
   setInput: SetInput
 }
 
-const Input = ({
-  handleInputChange,
-  handleSubmit,
-  input,
-  setInput,
-}: ChatInputProps) => {
-  return (
-    <div className="absolute bottom-0 left-0 z-10 w-full bg-gray-900">
-      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
-        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
-          <div className="relative flex w-full flex-grow flex-col p-4">
-            <form onSubmit={handleSubmit} className="relative">
-              <Textarea
-                rows={4}
-                autoFocus
-                onChange={handleInputChange}
-                value={input}
-                onKeyDown={e => {
-                  if (e.key === 'Enter' && !e.shiftKey) {
-                    e.preventDefault()
-                    handleSubmit()
-                    setInput('')
-                  }
-                }}
-                placeholder="Enter your question..."
-                className="resize-none rounded-xl bg-gray-800 text-base text-white hover:bg-gray-900"
-              />
+const Input = memo(
+  ({ handleInputChange, handleSubmit, input, setInput }: ChatInputProps) => {
+    const onSubmit = (e: React.FormEvent) => {
+      e.preventDefault()
+      handleSubmit(e)
+      setInput('')
+    }
 
-              <Button
-                size="sm"
-                type="submit"
-                className="absolute bottom-2 right-2 z-10 border border-border bg-gray-900"
-              >
-                <Send className="size-4" />
-              </Button>
-            </form>
+    return (
+      <div className="absolute bottom-0 left-0 z-10 w-full bg-gray-900">
+        <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+          <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+            <div className="relative flex w-full flex-grow flex-col p-4">
+              <form onSubmit={onSubmit} className="relative">
+                <Textarea
+                  rows={4}
+                  autoFocus
+                  onChange={handleInputChange}
+                  value={input}
+                  onKeyDown={e => {
+                    if (e.key === 'Enter' && !e.shiftKey) {
+                      e.preventDefault()
+                      onSubmit(e)
+                    }
+                  }}
+                  placeholder="Enter your question..."
+                  className="resize-none rounded-xl bg-gray-800 text-base text-white hover:bg-gray-900"
+                />
+
+                <Button
+                  size="sm"
+                  type="submit"
+                  className="absolute bottom-2 right-2 z-10 border border-border bg-gray-900"
+                >
+                  <Send className="size-4" />
+                </Button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  },
+)
+
+Input.displayName = 'Input'
 
 export default Input
