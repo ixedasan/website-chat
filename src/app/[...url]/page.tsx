cf171276c58e5b3b7d799f26ff7443c0b2647698
ixedasan <pandikgenshin@gmail.com>
2024-10-21 21:46:55 +0300

    feat(db): implement saving chat session

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,37 +1,42 @@
+import { cookies } from 'next/headers'
+
 import { ragChat } from '@/lib/rag-chat'
 import { redis } from '@/lib/redis'
 import Chat from '@/components/Chat'
 
-interface Props {
+type Props = {
   params: {
     url: string | string[] | undefined
   }
 }
 
-const remodelUrl = ({ url }: { url: string[] }) => {
-  const decodedUrl = url.map(part => decodeURIComponent(part))
-
-  return decodedUrl.join('//')
+const parseUrl = ({ url }: { url: string[] }): string => {
+  return url.map(segment => decodeURIComponent(segment)).join('//')
 }
 
 const Page = async ({ params }: Props) => {
-  console.log(params)
-  const remodeledUrl = remodelUrl({ url: params.url as string[] })
-  const isIndexed = await redis.sismember('indexed-urls', remodeledUrl)
+  const userSession = cookies().get('sessionId')?.value
+  const fullUrl = parseUrl({ url: params.url as string[] })
+  const chatId = (fullUrl + '--' + userSession).replace(/\//g, '')
 
-  const sessionId = 'mock-id'
+  const urlExists = await redis.sismember('indexed-urls', fullUrl)
 
-  if (!isIndexed) {
+  const messageHistory = await ragChat.history.getMessages({
+    amount: 10,
+    sessionId: chatId,
+  })
+
+  if (!urlExists) {
     await ragChat.context.add({
       type: 'html',
-      source: remodeledUrl,
+      source: fullUrl,
       config: { chunkOverlap: 50, chunkSize: 200 },
     })
 
-    await redis.sadd('indexed-urls', remodeledUrl)
+    await redis.sadd('indexed-urls', fullUrl)
   }
 
-  return <Chat sessionId={sessionId} initialMessages={[]} />
+  return <Chat sessionId={chatId} initialMessages={messageHistory} />
 }
 
 export default Page
